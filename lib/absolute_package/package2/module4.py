fd1c95ddfd72d0d68f7bad655762d7579c8ce6b4
leon <onditileon21@gmail.com>
2024-03-07 23:00:29 +0300

    completed

---
+++ lib/absolute_package/package2/module4.py
@@ -0,0 +1,22 @@
+# class Song:
+#      count = 0
+#      artists = []
+#      song_added = {}
+     
+#      def __init__(self, artist, name):
+#          self.artist = artist
+#          self.name = name
+#          self.songs_added = Song.song_added
+
+#      @classmethod
+#      def song_added(cls, artist, name):
+#         if artist in cls.song_added:
+#             cls.song_added([artist]).append(name)
+#         else:
+#             cls.song_added[artist] = [name]
+
+#     #  @classmethod
+#     #  def add_artist(cls):
+#     #      cls.add_artist.append
+# Song.song_added('Burnaboy', 'Diamond')
+# Song.add_artist('Amunga')
